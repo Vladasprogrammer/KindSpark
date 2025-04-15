d07e6172c373d59f6d96c32fc5c8bb8788150111
Vladasprogrammer <voroninvlad3@gmail.com>
2025-04-15 20:49:18 +0300

    not sure if that is necessary, but I will keep it just to be sure.

--- src/main.jsx
+++ src/main.jsx
@@ -3,7 +3,9 @@ import { BrowserRouter } from "react-router";
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.scss';
+import axios from 'axios';
 
+axios.defaults.withCredentials = true;
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
