eb8d99dbdde638a2ca8804aadbe64ad5c24dd9ab
Vladasprogrammer <voroninvlad3@gmail.com>
2025-04-16 16:04:10 +0300

    Deleted axios

--- src/main.jsx
+++ src/main.jsx
@@ -3,9 +3,7 @@ import { BrowserRouter } from "react-router";
 import { createRoot } from 'react-dom/client';
 import App from './App.jsx';
 import './index.scss';
-import axios from 'axios';
 
-axios.defaults.withCredentials = true;
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
     <App />
