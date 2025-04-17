8c054cba083db4d066e99d9a74c6044077a7e4f6
Vladasprogrammer <voroninvlad3@gmail.com>
2025-04-17 18:27:10 +0300

    Had before the 'approvedStoryIds' changed it to 'storyId'.
    Changed the amount: max to 20000.
    
    user_id is set to max 10, not 5

--- seeder/donation.js
+++ seeder/donation.js
@@ -1,12 +1,11 @@
 import { faker } from "@faker-js/faker";
 
-export function createDonation(approvedStoryIds) {
-  const storyId = faker.helpers.arrayElement(approvedStoryIds)
+export function createDonation(storyId) {
   return {
     donor_name: faker.person.firstName(),
-    amount: faker.number.int({ min: 5, max: 500 }),
+    amount: faker.number.int({ min: 5, max: 20000 }),
     story_id: storyId,
     created_at: faker.date.recent(),
-    user_id: faker.number.int({ min: 1, max: 5 })
+    user_id: faker.number.int({ min: 1, max: 10 })
   };
 }
