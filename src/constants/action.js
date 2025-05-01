754fc0cbdf20c531c4266889ab8334302d841926
Vladasprogrammer <voroninvlad3@gmail.com>
2025-05-01 21:33:15 +0300

    Actions basic ones.

--- src/constants/action.js
+++ src/constants/action.js
@@ -1,32 +1,16 @@
 // Users 1000 - 1999
 
-export const LOAD_ACTIVE_USERS_FROM_SERVER = 1000; // cia priliginimas yra visiskai nieko nedaro, nes vistiek veliau keisim post numeri as taip suprantu.
+export const LOAD_ACTIVE_USERS_FROM_SERVER = 1000; 
 
-// Posts 2000 - 2999
-export const LOAD_POSTS_FROM_SERVER = 2000;
+export const REGISTER_USERS = 1001;
 
-export const UP_VOTE_POST = 2101;
-export const DOWN_VOTE_POST = 2102;
+// Stories 2000 - 2999
+export const LOAD_STORIES_FROM_SERVER = 2000;
 
-export const ADD_NEW_POST = 2103;
+export const ADD_NEW_STORY = 2001;
 
-export const POST_UUID_TO_ID =  2104;
+export const STORY_UUID_TO_ID =  2002;
 
-// Comments 3000 - 3999
 
-export const LOAD_POST_COMMENTS = 3000;
-
-export const HIDE_POST_COMMENTS = 3001;
-export const SHOW_POST_COMMENTS = 3002;
-
-export const ADD_POST_COMMENT = 3003;
-
-export const DELETE_POST_COMMENT = 3004;
-
-// Chats 4000 - 4999
-
-export const LOAD_CHAT_USERS = 4000;
-
-export const LOAD_CHAT_MESSAGES = 4001;
 
 
