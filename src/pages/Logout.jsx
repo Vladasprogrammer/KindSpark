58452499ddc75af7f4793fc88d28e75ed4a29ac9
Vladasprogrammer <voroninvlad3@gmail.com>
2025-05-01 22:14:55 +0300

    Working Logout.

--- src/pages/Logout.jsx
+++ src/pages/Logout.jsx
@@ -1,12 +1,30 @@
+import { useEffect, useContext } from 'react';
+import { useNavigate } from 'react-router';
+import axios from 'axios';
+import * as C from '../constants/main';
+import Auth from '../contexts/Auth';
 
 
 export default function Logout() {
 
+  const navigate = useNavigate();
+  const { setUser } = useContext(Auth);
+
+  useEffect(_ => {
+    axios.post(C.SERVER_URL + 'logout', {}, { withCredentials: true })
+      .then(res => {
+        setUser(res.data.user);
+        navigate(C.GO_AFTER_LOGOUT);
+      })
+      .catch(err => {
+        console.log(err);
+      });
+
+  }, []);
+
+
   return (
-    <>
-    <div>
-      Logout?
-    </div>
-    </>
-  )
-}
+    <span>Atsijungiame...</span>
+  );
+
+};
