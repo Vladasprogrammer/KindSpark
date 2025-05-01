2ac79206c2cad061cbbf10aeba1d7d9794e15f46
Vladasprogrammer <voroninvlad3@gmail.com>
2025-05-01 22:18:20 +0300

    Yes, that is working.

--- src/hooks/useUsers.jsx
+++ src/hooks/useUsers.jsx
@@ -1,15 +1,20 @@
 import { useEffect, useReducer } from "react";
 import * as C from '../constants/main';
+import * as A from '../constants/action';
 import axios from 'axios';
+import usersReducer from "../reducers/usersReducer";
 
 
 export default function useUsers() {
-  const [users, dispatchUsers] = useReducer(null);
-
+  const [users, dispatchUsers] = useReducer(usersReducer, null);
+  
   useEffect(_ => {
-    axios.get(C.SERVER_URL + 'active-users-list')
+    axios.get(C.SERVER_URL + 'users/active-list')
     .then(res => {
-      console.log(res.data);
+      dispatchUsers({
+        type: A.LOAD_ACTIVE_USERS_FROM_SERVER,
+        payload: res.data.db
+      });
     })
     .catch(err => {
       console.log(err);
