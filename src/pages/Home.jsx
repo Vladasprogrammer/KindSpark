c15e74ce9a6f79c568113d7328477aee2490d0b2
Vladasprogrammer <voroninvlad3@gmail.com>
2025-05-10 08:26:57 +0300

    Simplified the file

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,11 +1,9 @@
 import React from 'react';
-import { Outlet } from 'react-router';
 
 export default function Home() {
   return (
     <div>
       <h1>Welcome to the Home Page</h1>
-      <Outlet />
     </div>
   );
 }
