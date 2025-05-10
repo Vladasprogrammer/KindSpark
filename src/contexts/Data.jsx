0faa4b644eaf30225d45f5bbff8a6d1d19642782
Vladasprogrammer <voroninvlad3@gmail.com>
2025-05-10 08:04:06 +0300

    Added hook useStories

--- src/contexts/Data.jsx
+++ src/contexts/Data.jsx
@@ -1,14 +1,18 @@
 import { createContext } from "react";
 import useUsers from "../hooks/useUsers";
+import useStories from "../hooks/useStories";
 
 const Data = createContext();
 
 export const DataProvider = ({ children }) => {
+
   const {users, dispatchUsers} = useUsers();
+  const {stories, dispatchStories} = useStories();
   
   return (
     <Data.Provider value={{
-      users, dispatchUsers
+      users, dispatchUsers,
+      stories, dispatchStories
     }}>
       { children }
     </Data.Provider>
