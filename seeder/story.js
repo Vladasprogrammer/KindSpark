9eaffcd9e5b9de3e840d7485e9963a180c361418
Vladasprogrammer <voroninvlad3@gmail.com>
2025-05-14 11:45:43 +0300

    Slightly updated generating images.

--- seeder/story.js
+++ seeder/story.js
@@ -7,9 +7,8 @@ export function createStory() {
     description: faker.lorem.paragraph(),
     goal_amount: faker.number.int({ min: 100, max: 999999999 }),
     current_amount: 0,
-    image: faker.image.url(),
+    image: faker.image.urlPicsumPhotos({ width: 1980, height: 1200, blur: 0, grayscale: false}),
     status: faker.helpers.arrayElement(['pending', 'approved', 'disapproved', 'completed']),
-    // user_id: faker.number.int({ min: 1, max: 5 }),
     created_at: faker.date.past()
   };
 }
