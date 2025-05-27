88b2ff9846cc7c79381dfdb290941eb6093dd27f
Vladasprogrammer <voroninvlad3@gmail.com>
2025-05-27 10:48:00 +0300

    Changed width and height

--- seeder/story.js
+++ seeder/story.js
@@ -7,7 +7,7 @@ export function createStory() {
     description: faker.lorem.paragraph(),
     goal_amount: faker.number.int({ min: 100, max: 999999999 }),
     current_amount: 0,
-    image: faker.image.urlPicsumPhotos({ width: 1980, height: 1200, blur: 0, grayscale: false}),
+    image: faker.image.urlPicsumPhotos({ width: 1920, height: 1080, blur: 0, grayscale: false}),
     status: faker.helpers.arrayElement(['pending', 'approved', 'disapproved', 'completed']),
     created_at: faker.date.past()
   };
