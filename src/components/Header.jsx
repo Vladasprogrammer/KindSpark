721f0a9790db73ad923f16b8047a974b19b4d08c
Vladasprogrammer <voroninvlad3@gmail.com>
2025-06-17 14:04:28 +0300

    Not sure that I should be doing it this way, by repeating the html ( DRY method ), but it works at least and I will go with it.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -19,14 +19,17 @@ export default function Header() {
   return (
     <header>
       <nav>
+        <div className="desktop-menu">
           <ul className="left">
             <li><NavLink to="/">Home</NavLink></li>
             <li><NavLink to="/stories">Stories</NavLink></li>
-            <li><NavLink to="/new-story">Share Your Story</NavLink></li>
+            <li><NavLink to="/new-story">Share your story</NavLink></li>
           </ul>
+
           <ul className="logo">
             <li><NavLink to="/">Kindspark</NavLink></li>
           </ul>
+
           <ul className="right">
             {
               user.role === 'guest' &&
@@ -41,7 +44,7 @@ export default function Header() {
                 <div className="right__avatar">
                   <img src={user.avatar} alt={user.name} />
                 </div>
-                <li className="right__username">{user.username}</li>
+                <div className="right__username">{user.username}</div>
                 <li><NavLink to='/logout' end>Logout</NavLink></li>
               </>
             }
@@ -52,6 +55,52 @@ export default function Header() {
               </>
             }
           </ul>
+        </div>
+
+        <div className="mobile-menu">
+          <input type="checkbox" id="menu-toggle"></input>
+
+          <label htmlFor="menu-toggle" className="hamburger">
+            <div className="line" />
+            <div className="line" />
+            <div className="line" />
+          </label>
+
+          <div className="modal">
+            <label htmlFor="menu-toggle" className="close">&#10006;</label>
+            <ul className="left">
+              <li><NavLink to="/">Home</NavLink></li>
+              <li><NavLink to="/stories">Stories</NavLink></li>
+              <li><NavLink to="/new-story">Share your story</NavLink></li>
+            </ul>
+
+            <ul className="right">
+              {
+                user.role === 'guest' &&
+                <>
+                  <li><NavLink to='/login' end>Login</NavLink></li>
+                  <li><NavLink to='/register' end>Register</NavLink></li>
+                </>
+              }
+              {
+                user.role !== 'guest' &&
+                <>
+                  <div className="right__avatar">
+                    <img src={user.avatar} alt={user.name} />
+                  </div>
+                  <div className="right__username">{user.username}</div>
+                  <li><NavLink to='/logout' end>Logout</NavLink></li>
+                </>
+              }
+              {
+                user.role === 'admin' &&
+                <>
+                  <li><NavLink to='/admin' end>Admin</NavLink></li>
+                </>
+              }
+            </ul>
+          </div>
+        </div>
       </nav>
     </header>
   );
