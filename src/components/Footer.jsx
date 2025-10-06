65d52dc7af0efa03da795447515672178ade5552
Vladasprogrammer <voroninvlad3@gmail.com>
2025-10-06 21:00:42 +0300

    Hiding footer

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -1,6 +1,14 @@
-import React from 'react';
+import { useLocation } from "react-router";
+import { HIDE_FOOTER_PATHS } from '../constants/main';
 
 function Footer() {
+
+  const { pathname } = useLocation();
+
+  if (HIDE_FOOTER_PATHS.includes(pathname)) {
+    return null;
+  }
+  
   return (
     <footer className='footer'>
       <p>&copy; 2025 KindSpark Fake Website</p>
