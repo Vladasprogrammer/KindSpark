f3aeb34a123fc6c6f2cb667b340f074f2570cda6
Vladasprogrammer <voroninvlad3@gmail.com>
2025-10-06 21:00:23 +0300

    Added imports for icons, Main component

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { AuthProvider } from './contexts/Auth';
 import Home from './pages/Home';
 import Header from './components/Header';
 import Footer from './components/Footer';
-import Page404 from './pages/Page404';
+import Page404 from './components/Page404';
 import Login from './pages/Login';
 import Logout from './pages/Logout';
 import Register from './pages/Register';
@@ -12,7 +12,16 @@ import Admin from './pages/Admin';
 import Stories from './pages/Stories';
 import NewStory from './pages/NewStory';
 import { DataProvider } from './contexts/Data';
-import Body from '../../socknet/src/Components/Body';
+import Body from './components/Body';
+import Main from './components/Main';
+
+
+import { library } from '@fortawesome/fontawesome-svg-core';
+import { fas } from '@fortawesome/free-solid-svg-icons';
+import { far } from '@fortawesome/free-regular-svg-icons';
+import { fab } from '@fortawesome/free-brands-svg-icons';
+
+library.add(fas, far, fab);
 
 export default function App() {
 
@@ -21,18 +30,20 @@ export default function App() {
       <DataProvider>
         <Body>
           <Header />
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path='new-story' element={<NewStory />} />
-            <Route path='stories' element={<Stories />} />
-            <Route path='login' element={<Login />} />
-            <Route path='logout' element={<Logout />} />
-            <Route path='register' element={<Register />} />
-            {/* <Route path='admin' element={
-          user?.role === 'admin' ? <Admin /> : <Navigate to="/login" />
-        } /> */}
-            <Route path='*' element={<Page404 />} />
-          </Routes>
+          <Main>
+            <Routes>
+              <Route index element={<Home />} />
+              <Route path='new-story' element={<NewStory />} />
+              <Route path='stories' element={<Stories />} />
+              <Route path='login' element={<Login />} />
+              <Route path='logout' element={<Logout />} />
+              <Route path='register' element={<Register />} />
+              {/* <Route path='admin' element={
+                user?.role === 'admin' ? <Admin /> : <Navigate to="/login" />
+                } /> */}
+              <Route path='*' element={<Page404 />} />
+            </Routes>
+          </Main>
           <Footer />
         </Body>
       </DataProvider>
